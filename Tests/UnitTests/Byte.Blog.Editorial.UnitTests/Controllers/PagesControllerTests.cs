b626d317f3e124d0f00f66f8bc5dc5ce8edacd5d
Ben Lakey <benlakey@gmail.com>
2012-05-03 16:58:53 -0700

    Adding unit tests for PagesController and PagesDashboardController

---
+++ Tests/UnitTests/Byte.Blog.Editorial.UnitTests/Controllers/PagesControllerTests.cs
@@ -0,0 +1,104 @@
+﻿using AutoMapper;
+using Byte.Blog.Content;
+using Byte.Blog.Editorial.Controllers;
+using Byte.Blog.Editorial.Models;
+using Byte.Blog.Framework.UnitTests;
+using Xunit;
+
+namespace Byte.Blog.Editorial.UnitTests.Controllers
+{
+    public class PagesControllerTests
+    {
+        [Fact]
+        public void Edit_pulls_page_from_database_and_creates_viewmodel()
+        {
+            Mapper.Reset();
+            AutoMapperConfig.RegisterMappings();
+
+            var store = new TestableStore();
+
+            var page = new Page
+            {
+                Id = "pages/123",
+                Title = "foo"
+            };
+
+            using (var pagesController = new PagesController(store))
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(page);
+                    session.SaveChanges();
+
+                    RavenControllerTestHelper.SetSessionOnController(pagesController, session);
+
+                    string uniquePart = page.Id.Replace(Page.IdPrefix, "");
+
+                    var actionResult = pagesController.Edit(uniquePart);
+                    var pageEditModel = ControllerTestHelper.GetModelInActionResult<PageEditModel>(actionResult);
+
+                    Assert.Equal(page.Title, pageEditModel.Title);
+                }
+            }
+
+            Mapper.Reset();
+        }
+
+        [Fact]
+        public void Save_persists_valid_page_to_database()
+        {
+            Mapper.Reset();
+            AutoMapperConfig.RegisterMappings();
+
+            string id = "foo/123";
+
+            var store = new TestableStore();
+
+            using (var pagesController = new PagesController(store))
+            {
+                using (var session = store.OpenSession())
+                {
+                    RavenControllerTestHelper.SetSessionOnController(pagesController, session);
+                    pagesController.Save(new PageEditModel() { Id = id, Title = "foo" });
+                }
+            }
+
+            Assert.True(store.Contains(id));
+
+            Mapper.Reset();
+        }
+
+        [Fact]
+        public void Delete_sets_page_deleted()
+        {
+            Mapper.Reset();
+            AutoMapperConfig.RegisterMappings();
+
+            var store = new TestableStore();
+
+            var page = new Page
+            {
+                Id = "pages/123",
+                Title = "foo"
+            };
+
+            using (var pagesController = new PagesController(store))
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(page);
+                    session.SaveChanges();
+
+                    RavenControllerTestHelper.SetSessionOnController(pagesController, session);
+
+                    pagesController.Delete(page.Id);
+
+                    var pageLoaded = session.Load<Page>(page.Id);
+                    Assert.True(pageLoaded.Deleted);
+                }
+            }
+
+            Mapper.Reset();
+        }
+    }
+}
---
+++ Tests/UnitTests/Byte.Blog.Editorial.UnitTests/Controllers/PagesDashboardControllerTests.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using AutoMapper;
+using Byte.Blog.Content;
+using Byte.Blog.Editorial.Controllers;
+using Byte.Blog.Editorial.Models;
+using Byte.Blog.Framework.UnitTests;
+using Raven.Client;
+using Xunit;
+
+namespace Byte.Blog.Editorial.UnitTests.Controllers
+{
+    public class PagesDashboardControllerTests
+    {
+        [Fact]
+        public void Dashboard_fetch_crafts_viewmodels_from_database()
+        {
+            Mapper.Reset();
+            AutoMapperConfig.RegisterMappings();
+
+            var store = new TestableStore();
+
+            int pagesCount = 2;
+
+            var pages = GetTestPages(pagesCount);
+            SaveTestPages(store, pages);
+
+            using (var pagesDashboardController = new PagesDashboardController(store))
+            {
+                using (var session = store.OpenSession())
+                {
+                    RavenControllerTestHelper.SetSessionOnController(pagesDashboardController, session);
+
+                    var queryModel = new PageDashboardQueryModel
+                    {
+                        PageNumber = 1,
+                        PageSize = 20
+                    };
+
+                    var actionResult = pagesDashboardController.Fetch(queryModel);
+                    var pageEditModels = GetPageEditModelsFromResult(actionResult);
+
+                    foreach (var page in pages)
+                    {
+                        Assert.True(pageEditModels.Any(editModel => editModel.Id == page.Id));
+                    }
+                }
+            }
+        }
+
+        private static IEnumerable<Page> GetTestPages(int number)
+        {
+            return Enumerable.Range(0, number)
+                .Select(n =>
+                    new Page
+                    {
+                        Id = "pages/" + n,
+                        Title = "page " + n
+                    });
+        }
+
+        private static void SaveTestPages(IDocumentStore store, IEnumerable<Page> pages)
+        {
+            using (var session = store.OpenSession())
+            {
+                foreach (var page in pages)
+                {
+                    session.Store(page);
+                }
+                session.SaveChanges();
+            }
+        }
+
+        private static IEnumerable<PageEditModel> GetPageEditModelsFromResult(ActionResult actionResult)
+        {
+            var jsonResult = actionResult as JsonResult;
+            if (jsonResult == null)
+            {
+                return Enumerable.Empty<PageEditModel>();
+            }
+
+            return jsonResult.Data as IEnumerable<PageEditModel>;
+        }
+    }
+}
