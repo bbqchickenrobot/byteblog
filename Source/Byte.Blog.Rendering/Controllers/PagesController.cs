8a0e75b987622ca8af1615b501f6938fba80ce73
Ben Lakey <benlakey@gmail.com>
2012-05-02 22:05:27 -0700

    Abstracting page fetching by slug to PageFactory.

--- Source/Byte.Blog.Rendering/Controllers/PagesController.cs
+++ Source/Byte.Blog.Rendering/Controllers/PagesController.cs
@@ -16,34 +16,25 @@ namespace Byte.Blog.Rendering.Controllers
 
         public ActionResult ViewPage(string pageSlug)
         {
-            Page page;
-
-            if (string.IsNullOrEmpty(pageSlug))
+            var pageFactory = new PageFactory(this.session);
+            var page = pageFactory.CreateFromSlug(pageSlug);
+            if (page == null)
             {
-                page = Page.HomePage;
-            }
-            else
-            {
-                page = this.session.Query<Page>()
-                    .FirstOrDefault(p => p.Slug == pageSlug);
-
-                if (page == null)
-                {
-                    return new HttpNotFoundResult("No such page.");
-                }
+                return new HttpNotFoundResult("No such page.");
             }
 
-            var entries = this.session.Query<Entry>()
+            var entryToEntryViewModelMapper = new EntryToEntryViewModelMapper();
+
+            var entryViewModels = this.session.Query<Entry>()
                 .Where(e => e.PageId == page.Id)
                 .OrderByDescending(e => e.PublishedAtUtc)
-                .ToList();
+                .ToList()
+                .Select(entryToEntryViewModelMapper.Map);
 
-            var entryCollectionViewModel = new EntryCollectionViewModel();
-
-            if (entries.Any())
+            var entryCollectionViewModel = new EntryCollectionViewModel
             {
-                entryCollectionViewModel.Entries = entries.Select(e => new EntryToEntryViewModelMapper().Map(e));
-            }
+                Entries = entryViewModels
+            };
 
             return this.View(entryCollectionViewModel);
         }
---
+++ Source/Byte.Blog.Rendering/Models/PageFactory.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Byte.Blog.Content;
+using Raven.Client;
+
+namespace Byte.Blog.Rendering.Models
+{
+    public class PageFactory
+    {
+        private readonly IDocumentSession session;
+
+        public PageFactory(IDocumentSession session)
+        {
+            this.session = session;
+        }
+
+        public Page CreateFromSlug(string slug)
+        {
+            if (string.IsNullOrEmpty(slug))
+            {
+                return Page.HomePage;
+            }
+
+            var page = this.session
+                .Query<Page>()
+                .FirstOrDefault(p => p.Slug == slug);
+
+            return page;
+        }
+    }
+}
