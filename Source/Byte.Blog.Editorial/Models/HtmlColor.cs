3f3b6bcbb228593500374868e36e0dd77fc007a2
Ben Lakey <benlakey@gmail.com>
2012-05-03 19:06:00 -0700

    Refactoring HtmlColor such that you can now refer to individual colors by name.

--- Source/Byte.Blog.Editorial/Models/HtmlColor.cs
+++ Source/Byte.Blog.Editorial/Models/HtmlColor.cs
@@ -3,15 +3,23 @@ namespace Byte.Blog.Editorial.Models
 {
     public class HtmlColor
     {
-        public static HtmlColor[] AllColors = new HtmlColor[]
+        public static readonly HtmlColor Aqua = new HtmlColor("Aqua", "#48D8FF");
+        public static readonly HtmlColor Purple = new HtmlColor("Purple","#A53AF3");
+        public static readonly HtmlColor Magenta = new HtmlColor("Magenta", "#FF0F8B");
+        public static readonly HtmlColor Red = new HtmlColor("Red", "#FF250D");
+        public static readonly HtmlColor Green = new HtmlColor("Green", "#19B917");
+        public static readonly HtmlColor Yellow = new HtmlColor("Yellow", "#FED20A");
+        public static readonly HtmlColor Blue = new HtmlColor("Blue", "#2F6EFC");
+
+        public static HtmlColor[] AllColors = new[]
         {
-            new HtmlColor("Aqua","#48D8FF"),
-            new HtmlColor("Purple","#A53AF3"),
-            new HtmlColor("Magenta","#FF0F8B"),
-            new HtmlColor("Red","#FF250D"),
-            new HtmlColor("Green","#19B917"),
-            new HtmlColor("Yellow","#FED20A"),
-            new HtmlColor("Blue","#2F6EFC")
+            Aqua,
+            Purple, 
+            Magenta,
+            Red,
+            Green,
+            Yellow,
+            Blue
         };
 
         public string FriendlyName { get; private set; }
