207d46f1b1f92e5c6fe9ef9a12c48530c4897e7b
Ben Lakey <benlakey@gmail.com>
2012-05-16 23:41:02 -0700

    Wired up timestamp updates for modifications and publishes. Added unit tests for EntryEditModelToEntryMapper and some minor refactorings.

--- Source/Byte.Blog.Editorial/Models/EntryEditModelToEntryMapper.cs
+++ Source/Byte.Blog.Editorial/Models/EntryEditModelToEntryMapper.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System;
+using AutoMapper;
 using Byte.Blog.Content;
 using Byte.Blog.Framework;
 using Raven.Client;
@@ -16,11 +17,70 @@ namespace Byte.Blog.Editorial.Models
 
         public Entry Map(EntryEditModel editModel)
         {
-            var entry = Mapper.Map<Entry>(editModel);
+            var newEntry = Mapper.Map<Entry>(editModel);
+            var existingEntry = this.GetExistingEntry(newEntry.Id);
 
-            entry.Slug = (new SlugMaker()).CreateSlug(entry.Title);
+            SetSlug(existingEntry, newEntry);
 
-            return entry;
+            this.UpdateTimestamps(existingEntry, newEntry);
+
+            return newEntry;
+        }
+
+        private Entry GetExistingEntry(string id)
+        {
+            if (string.IsNullOrEmpty(id) || id == Entry.IdPrefix)
+            {
+                return null;
+            }
+
+            return this.session.Load<Entry>(id);
+        }
+
+        private static void SetSlug(Entry existingEntry, Entry newEntry)
+        {
+            if (existingEntry == null)
+            {
+                var slugMaker = new SlugMaker();
+                newEntry.Slug = slugMaker.CreateSlug(newEntry.Title);
+            }
+            else
+            {
+                newEntry.Slug = existingEntry.Slug;
+            }
+        }
+
+        private void UpdateTimestamps(Entry existingEntry, Entry newEntry)
+        {
+            UpdateLastModifiedAtUtc(newEntry);
+            UpdatePublishedAtUtc(existingEntry, newEntry);
+        }
+
+        private static void UpdateLastModifiedAtUtc(Entry entry)
+        {
+            entry.LastModifiedAtUtc = DateTimeOffset.UtcNow;
+        }
+
+        private static void UpdatePublishedAtUtc(Entry existingEntry, Entry newEntry)
+        {
+            if (IsBecomingPublished(existingEntry, newEntry))
+            {
+                newEntry.PublishedAtUtc = DateTimeOffset.UtcNow;
+            }
+            else if (existingEntry != null)
+            {
+                newEntry.PublishedAtUtc = existingEntry.PublishedAtUtc;
+            }
+        }
+
+        private static bool IsBecomingPublished(Entry existingEntry, Entry newEntry)
+        {
+            if (newEntry.Published && (existingEntry == null || !existingEntry.Published))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
---
+++ Tests/UnitTests/Byte.Blog.Editorial.UnitTests/Models/EntryEditModelToEntryMapperTests.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using AutoMapper;
+using Byte.Blog.Content;
+using Byte.Blog.Editorial.Models;
+using Byte.Blog.Framework.UnitTests;
+using Raven.Client;
+using Xunit;
+
+namespace Byte.Blog.Editorial.UnitTests.Models
+{
+    public class EntryEditModelToEntryMapperTests
+    {
+        [Fact]
+        public void New_database_entry_has_slug_set_based_on_title()
+        {
+            Mapper.Reset();
+            AutoMapperConfig.RegisterMappings();
+
+            var testableStore = new TestableStore();
+
+            var entryEditModel = new EntryEditModel();
+            entryEditModel.Title = "Foo Title";
+
+            string expectedSlug = "foo-title";
+
+            using (var session = testableStore.OpenSession())
+            {
+                var entry = GetMappedEntryFor(session, entryEditModel);
+
+                Assert.Equal(expectedSlug, entry.Slug);
+            }
+
+            Mapper.Reset();
+        }
+
+        [Fact]
+        public void Existing_database_entry_slug_remains_unchanged()
+        {
+            Mapper.Reset();
+            AutoMapperConfig.RegisterMappings();
+
+            var testableStore = new TestableStore();
+
+            var entry = new Entry
+            {
+                Slug = "foo-title"
+            };
+
+            using (var session = testableStore.OpenSession())
+            {
+                session.Store(entry);
+                session.SaveChanges();
+
+                var entryEditModel = new EntryEditModel
+                {
+                    Id = entry.Id,
+                    Title = "Bar Title"
+                };
+
+                var entryMapped = GetMappedEntryFor(session, entryEditModel);
+
+                Assert.Equal(entry.Slug, entryMapped.Slug);
+            }
+
+            Mapper.Reset();
+        }
+
+        [Fact]
+        public void Last_modified_date_updates()
+        {
+            Mapper.Reset();
+            AutoMapperConfig.RegisterMappings();
+
+            var testableStore = new TestableStore();
+
+            var entryEditModel = new EntryEditModel
+            {
+                LastModifiedAtUtc = DateTimeOffset.MinValue
+            };
+
+            using (var session = testableStore.OpenSession())
+            {
+                var entry = GetMappedEntryFor(session, entryEditModel);
+
+                Assert.NotEqual(entryEditModel.LastModifiedAtUtc, entry.LastModifiedAtUtc);
+            }
+
+            Mapper.Reset();
+        }
+
+        [Fact]
+        public void Existing_entry_already_published_leaves_published_date_unchanged()
+        {
+            Mapper.Reset();
+            AutoMapperConfig.RegisterMappings();
+
+            var testableStore = new TestableStore();
+
+            var entry = new Entry
+            {
+                PublishedAtUtc = DateTimeOffset.UtcNow.AddDays(-1)
+            };
+
+            using (var session = testableStore.OpenSession())
+            {
+                session.Store(entry);
+                session.SaveChanges();
+
+                var entryEditModel = new EntryEditModel
+                {
+                    Id = entry.Id,
+                    PublishedAtUtc = DateTimeOffset.UtcNow
+                };
+
+                var entryMapped = GetMappedEntryFor(session, entryEditModel);
+
+                Assert.Equal(entry.PublishedAtUtc, entryMapped.PublishedAtUtc);
+            }
+
+            Mapper.Reset();
+        }
+
+        [Fact]
+        public void Existing_entry_not_published_until_now_updates_published_date()
+        {
+            Mapper.Reset();
+            AutoMapperConfig.RegisterMappings();
+
+            var testableStore = new TestableStore();
+
+            var entry = new Entry();
+
+            using (var session = testableStore.OpenSession())
+            {
+                session.Store(entry);
+                session.SaveChanges();
+
+                var entryEditModel = new EntryEditModel
+                {
+                    Id = entry.Id,
+                    Published = true
+                };
+
+                var entryMapped = GetMappedEntryFor(session, entryEditModel);
+
+                Assert.NotEqual(entry.PublishedAtUtc, entryMapped.PublishedAtUtc);
+            }
+
+            Mapper.Reset();
+        }
+
+        [Fact]
+        public void New_unpublished_entry_leaves_published_date_unchanged()
+        {
+            Mapper.Reset();
+            AutoMapperConfig.RegisterMappings();
+
+            var testableStore = new TestableStore();
+
+            var entry = new Entry();
+
+            using (var session = testableStore.OpenSession())
+            {
+                session.Store(entry);
+                session.SaveChanges();
+
+                var entryEditModel = new EntryEditModel
+                {
+                    Id = entry.Id
+                };
+
+                var entryMapped = GetMappedEntryFor(session, entryEditModel);
+
+                Assert.Equal(entry.PublishedAtUtc, entryMapped.PublishedAtUtc);
+            }
+
+            Mapper.Reset();
+        }
+
+        private static Entry GetMappedEntryFor(IDocumentSession session, EntryEditModel entryEditModel)
+        {
+            var entryEditModelToEntryMapper = new EntryEditModelToEntryMapper(session);
+
+            return entryEditModelToEntryMapper.Map(entryEditModel);
+        }
+    }
+}
